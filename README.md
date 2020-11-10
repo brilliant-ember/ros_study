68b9ad980321099511efb221520ee102fd13d17d
mo <fire.senfony@gmail.com>
2020-11-10 18:55:09 -0500

    added few comment on making the frame progress with time and the readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ros_study
+for the learning_tf package I am following this 
+https://wiki.ros.org/urdf/Tutorials
+
+to see broad casted reference frames use 
+'''
+    rosrun tf tf_echo /turtle1 /turtle2
+'''
--- ROS_basics_ws/src/learning_tf/src/frame_tf_broadcaster.cpp
+++ ROS_basics_ws/src/learning_tf/src/frame_tf_broadcaster.cpp
@@ -19,6 +19,12 @@ int main(int argc, char** argv){
         */
         transform.setOrigin(tf::Vector3(0.0, 2.0, 0.0));
         transform.setRotation(tf::Quaternion(0,0,0,1));
+        // The above transform is a fixed frame, if we want the frame to change
+        // with time we can do this
+        /*
+            transform.setOrigin( tf::Vector3(2.0*sin(ros::Time::now().toSec()), 2.0*cos(ros::Time::now().toSec()), 0.0) );
+            transform.setRotation( tf::Quaternion(0, 0, 0, 1) );    
+        */
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(),
          "turtle1", "carrot1"));
 
